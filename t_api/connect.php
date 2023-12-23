9371ee5fa66ef14e0b56fe9f54cb612fb6d6e7d1
AlSaidReem <144600484+AlSaidReem@users.noreply.github.com>
2023-12-23 14:21:59 +0300

    Delete t_api directory

--- t_api/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname ="tourism_agency";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
